f6c93e095075f47f9567510fd903633a1186b016
aquarajesh <rajesh@aquaexchange.com>
2024-08-03 15:06:54 +0530

    mainfile added

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import fetch_yesterday_count as fetch_yesterday_count
 app = Flask(__name__)
 
 @app.route('/send_checktrayCount_email', methods=['GET'])
-def call_function():
+def send_checktrayCount_email():
     data =  fetch_yesterday_count.update_check_tray_images_count()
     return jsonify({"message": data})
 if __name__ == '__main__':
