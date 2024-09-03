7d2c1175164afc3d008e01e3b9157dc740faf5e8
aquarajesh <rajesh@aquaexchange.com>
2024-09-03 13:38:39 +0530

    add send email form

--- main.py
+++ main.py
@@ -42,7 +42,39 @@ def send_email():
            return jsonify({"error": response}),500
     except Exception:
             return jsonify({"error": "Error sending email"}), 500
-    
+@app.route('/send_email_by_formdata', methods=['POST'])
+def send_email_by_formdata():
+    try:
+        # Extract data from form fields
+        subject = request.form.get('subject', "***subject***")
+        msgbody = request.form.get('msgbody', "***msgbody***")
+        to_emails = request.form.getlist('to_emails')  # Assuming multiple emails can be provided
+        cc_emails = request.form.getlist('cc_emails')  # Assuming multiple emails can be provided
+        from_email = request.form.get('from_email', 'developer@nextaqua.in')
+        app_password = request.form.get('app_password', 'zdkc wler hovo jclu')
+
+        # Extract file data if provided
+        attachment = request.files.get('attachment')  # Extract the file from the form data
+        attachment_name = attachment.filename if attachment else None
+        attachment_data = attachment.read() if attachment else None
+        attachment_mime_type = attachment.mimetype if attachment else None
+
+        # Call the email sending service
+        response, isSent = sendemail_service.send_email(
+            subject, msgbody, to_emails, cc_emails, 
+            from_email=from_email, appPassword=app_password,
+            attachment_name=attachment_name, 
+            attachment_data=attachment_data, 
+            attachment_type=attachment_mime_type
+        )
+
+        # Return the appropriate response
+        if isSent:
+            return jsonify({"message": response}), 200
+        else:
+            return jsonify({"error": response}), 500
+    except Exception as e:
+        return jsonify({"error": f"Error sending email: {str(e)}"}), 500   
 
 # @app.route('/notify_day_feed_bubble', methods=['POST'])
 # def notifyDayFeedBubble():
